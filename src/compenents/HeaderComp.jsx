60f0fdd07079cb7af8edc3fd205fe5e2e9dfc1e2
stephen4018 <stephenohaeri4018@gmail.com>
2022-11-02 09:53:13 +0100

    initial setup

---
+++ src/compenents/HeaderComp.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const HeaderComp = () => {
+  return (
+    <div className="header-container">
+      <div className="header-container-inner">
+        <div className="image-container">
+          <img
+            src="https://blackincardio805833486.files.wordpress.com/2020/10/aaprofilepic1-annette-ansong.jpg?w=539"
+            alt=""
+          />
+          <div className="hover"></div>
+        </div>
+        <div className="profile-name">
+          <p>Annette Black</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderComp;
